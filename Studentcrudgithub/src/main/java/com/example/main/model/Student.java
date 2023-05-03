a7314a3702fd277ec07d9ba29b9caeae01949d64
Pooo1216 <channapooja07@gmail.com>
2023-05-03 17:36:57 +0530

    student class change

--- Studentcrudgithub/src/main/java/com/example/main/model/Student.java
+++ Studentcrudgithub/src/main/java/com/example/main/model/Student.java
@@ -1,15 +1,20 @@
 package com.example.main.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Data
 @Setter
 @Getter
 @Entity
 public class Student 
 {
+	@Id
+	
 	private int id;
 	private String sname;
 	private String mobno;
