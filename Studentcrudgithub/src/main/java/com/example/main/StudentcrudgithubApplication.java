66df66095f62f73601d72f88a5e4f131067a9fbe
Pooo1216 <channapooja07@gmail.com>
2023-05-03 16:46:31 +0530

    initial commit

---
+++ Studentcrudgithub/src/main/java/com/example/main/StudentcrudgithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentcrudgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentcrudgithubApplication.class, args);
+	}
+
+}
---
+++ Studentcrudgithub/src/test/java/com/example/main/StudentcrudgithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentcrudgithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
