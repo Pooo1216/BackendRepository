eaa3b054d739363dcb082c582e0da8a74baa47f5
Pooo1216 <channapooja07@gmail.com>
2023-05-03 14:37:48 +0530

    first project

---
+++ SppuAdmission/src/main/java/com/sppu/main/SppuAdmissionApplication.java
@@ -0,0 +1,13 @@
+package com.sppu.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SppuAdmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SppuAdmissionApplication.class, args);
+	}
+
+}
